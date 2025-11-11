fb8e78af609b58d4d7c26da746c52d669ee1ff4a
Sávio Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2025-11-11 01:26:41 +0000

    :recycle: REFACTOR: Alterado exibição da mensagem final

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Mãe, você é a pessoa mais incrivel que eu conheço! NEOQEAV!</h1>
+    return (<body style={{textAlign: 'center'}}>
+                <h1>Mãe, você é a pessoa mais incrivel que eu conheço</h1>
+                <strong style={{fontSize: '18px'}}>NEOQEAV!</strong>
+                <br></br>
+                <img src="https://i.pinimg.com/originals/b3/a2/ef/b3a2efc0167431c879bf62ac9ac00a23.gif"></img>
+            </body>)
 }
 
 export default Home;
