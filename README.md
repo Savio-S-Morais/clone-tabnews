7df0103a09f1a30ac20686f78be501ce428ac53f
Sávio Morais <101912070+Savio-S-Morais@users.noreply.github.com>
2025-11-14 00:23:56 +0000

    :heavy_plus_sign: BUILD: Adicionado scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aplicação dos conhecimentos adquiridos no https://curso.dev, com a implementação de uma cópia do http://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 function Home() {
-    return (<body style={{textAlign: 'center'}}>
-                <h1>Mãe, você é a pessoa mais incrivel que eu conheço</h1>
-                <strong style={{fontSize: '18px'}}>NEOQEAV!</strong>
-                <br></br>
-                <img src="https://i.pinimg.com/originals/b3/a2/ef/b3a2efc0167431c879bf62ac9ac00a23.gif"></img>
-            </body>)
+  return (
+    <body style={{ textAlign: "center" }}>
+      <h1>Mãe, você é a pessoa mais incrivel que eu conheço</h1>
+      <strong style={{ fontSize: "18px" }}>NEOQEAV!</strong>
+      <br></br>
+      <img src="https://i.pinimg.com/originals/b3/a2/ef/b3a2efc0167431c879bf62ac9ac00a23.gif"></img>
+    </body>
+  );
+}
+
+function teste() {
+  console.log("identacao errada");
 }
 
 export default Home;
